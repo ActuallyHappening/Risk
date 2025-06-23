5f8ea13a99d74a07e80f6f72ac6348ee45b4b328
Caleb Yates <calebyates42@gmail.com>
2025-06-23 16:12:59 +1000

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,6 @@
 #[path = "tracing.rs"]
 pub mod app_tracing;
+
+pub mod prelude {
+	pub use tracing::{debug, error, info, trace, warn};
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use risk::app_tracing;
+use risk::prelude::*;
+
+fn main() -> color_eyre::Result<()> {
+	app_tracing::install_tracing("")?;
+	trace!("Started tracing");
+
+	Ok(())
 }
